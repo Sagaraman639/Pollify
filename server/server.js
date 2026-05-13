28fc63d9064bfb1085fb6d5d34fc435391cdc369
Sagaraman639 <sagaraman369@gmail.com>
2026-05-14 02:24:52 +0530

    Production backend setup

--- server/server.js
+++ server/server.js
@@ -23,17 +23,19 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"]
   }
 });
 
 app.set("io", io);
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
